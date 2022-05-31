b7d764dd537708a110a2e22469205dd487f6f56c
shshkr09 <63920849+shshkr09@users.noreply.github.com>
2022-05-31 13:59:33 +0530

    final

--- create_pipeline.sql
+++ create_pipeline.sql
@@ -1,3 +1,6 @@
+# Modify the ip address with you kafka server IP
+#SELECT TO_JSON(loan_disburse.*) FROM loan_disburse where cust_ids=cid AND loan_id=lid INTO KAFKA '44.77.302.01:9092/disbursement';
+#
 use appws;
 CREATE PIPELINE kaf_loanreq
 AS LOAD DATA KAFKA '<Ip address of kafk>:9092/loan_request'
